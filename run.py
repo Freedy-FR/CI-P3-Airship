28f6708787ef1c33e0e5b8890a79ce812278c6ab
Freedy-FR <freedy.ramos@hotmail.com>
2023-01-21 00:01:34 +0000

    feat: :sparkles: Add UsernameInput class and Welcome class

--- run.py
+++ run.py
@@ -1,1 +1,53 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+class UsernameInput:
+    """
+    Ask for username input and validate name
+    """
+    def input_name():
+        name = input("Please tell me your name captain?\n")        
+        while True:
+            if len(name) == 0:
+                print("We are going to need your name for this adventure captain!")
+                name = input("Please tell me your name captain?\n")                
+            else:
+                return name
+
+
+class Welcome: 
+    def welcome_screen(player_name):
+        """
+        Show welcome screen and message to the user
+        """
+        print('''                      . ___
+                    __,' __`.                _..----....____
+        __...--.'``;.   ,.   ;``--..__     .'    ,-._    _.-'
+  _..-''-------'   `'   `'   `'     O ``-''._   ,;') _,'
+,'________________                          \`-._`-','
+ `._              ```````````------...___   '-.._'-:
+    ```--.._      ,.                     ````--...__\-.
+            `.--. `-`                       ____    |  |`
+              `. `.                       ,'`````.  ;  ;`
+                `._`.        __________   `.      \'__/`
+                   `-:._____/______/___/____`.     \  `
+                               |       `._    `.    /
+                               `._________`-.   `.   `.___
+                                                  `------ ''')
+
+        print(f" {player_name} Welcome to Airship battles!")
+
+
+
+
+
+
+
+
+def RunGame():
+
+    username = UsernameInput.input_name()    
+    Welcome.welcome_screen(username)
+
+
+
+RunGame()
